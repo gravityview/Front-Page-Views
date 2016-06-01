ff27486ce7b574ec521a1f10ed598fb207efac87
Zack Katz <zack@katzwebservices.com>
2016-06-01 14:54:09 -0600

    Code spacing & docblocks

--- gravityview-frontpage-views.php
+++ gravityview-frontpage-views.php
@@ -32,35 +32,47 @@ add_filter( 'parse_query', 'gv_fix_frontpage_parse_query', 10, 1 );
  * @link  https://core.trac.wordpress.org/ticket/23867 Fixes this core issue
  * @link https://wordpress.org/plugins/cpt-on-front-page/ Code is based on this
  *
+ * @param WP_Query &$query (passed by reference)
+ *
+ * @return void
  */
 function gv_fix_frontpage_parse_query( $query ) {
-	$qv =& $query->query_vars;
+	global $wp_rewrite;
+
 	if ( ( $query->is_home || $query->is_page ) && 'page' === get_option('show_on_front') && $page_id = get_option('page_on_front') ) {
 
-		$_query = wp_parse_args($query->query);
+		$_query = wp_parse_args( $query->query );
+
 		// pagename can be set and empty depending on matched rewrite rules. Ignore an empty pagename.
 		if ( isset( $_query['pagename'] ) && '' == $_query['pagename'] ) {
 			unset( $_query['pagename'] );
 		}
 
 		// this is where will break from core wordpress
-		$ignore = array('preview', 'page', 'paged', 'cpage');
-		global $wp_rewrite;
-		foreach( $wp_rewrite->endpoints as $endpoint ) {
+		$ignore = array( 'preview', 'page', 'paged', 'cpage' );
+		foreach ( $wp_rewrite->endpoints as $endpoint ) {
 			$ignore[] = $endpoint[1];
 		}
 
-		if ( empty($_query) || !array_diff( array_keys($_query), $ignore ) ) {
+		// Modify the query if:
+		// - We're on the "Page on front" page (which we are), and:
+		// - The query is empty OR
+		// - The query includes keys that are associated with registered endpoints. `entry`, for example.
+		if ( empty( $_query ) || ! array_diff( array_keys( $_query ), $ignore ) ) {
+
+			$qv =& $query->query_vars;
+
 			$query->is_page = true;
 			$query->is_home = false;
+			$qv['page_id']  = $page_id;
 
-			$qv['page_id'] = get_option('page_on_front');
 			// Correct <!--nextpage--> for page_on_front
-			if ( !empty($qv['paged']) ) {
-			  $qv['page'] = $qv['paged'];
-			  unset($qv['paged']);
+			if ( ! empty( $qv['paged'] ) ) {
+				$qv['page'] = $qv['paged'];
+				unset( $qv['paged'] );
 			}
 		}
+
 	}
 
 	// reset the is_singular flag after our updated code above
